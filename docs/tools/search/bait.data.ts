4f16f2366c157b1acff0a693d6eb1371d6714aea
DannyPX <47347980+DannyPX@users.noreply.github.com>
2025-09-30 13:44:17 +0200

    Update bait.data.ts

--- docs/tools/search/bait.data.ts
+++ docs/tools/search/bait.data.ts
@@ -237,7 +237,6 @@ export default {
           "Eels"
         ],
         "fishspecies": [],
-        "fishspecies": [],
         "lifestyle": [],
         "location": [],
         "water": "Freshwater"
